298dfe2708fbf54ad6a505710d1db4eba9b667db
The-Abe <abevanderwielen@gmail.com>
2016-04-13 14:51:02 +0200

    Allow the timer length to be set as a parameter.

--- lib/pommy.rb
+++ lib/pommy.rb
@@ -24,7 +24,7 @@ module Pommy
 
   def draw_bar(percentage, text)
     width = Pommy::get_size[:width]
-    negative_percentage = 100 - percentage
+    negative_percentage = 100.0 - percentage
     done_blocks = (negative_percentage/100.0*width).round
     undone_blocks = width-done_blocks
     green_text = text[0,done_blocks]
@@ -33,40 +33,38 @@ module Pommy
     done_blocks = 0 if done_blocks < 0
     undone_blocks = undone_blocks - red_text.length
     undone_blocks = 0 if undone_blocks < 0
-    Curses.attron(Curses.color_pair(Curses::COLOR_GREEN)|Curses::A_NORMAL){
+    Curses.attron(Curses.color_pair(Curses::COLOR_GREEN)|Curses::A_BOLD){
       Curses.addstr "#{green_text}#{" "*done_blocks}"
     }
-    Curses.attron(Curses.color_pair(Curses::COLOR_RED)|Curses::A_NORMAL){
+    Curses.attron(Curses.color_pair(Curses::COLOR_RED)|Curses::A_BOLD){
       Curses.addstr "#{red_text}#{" "*undone_blocks}"
     }
   end
 
-  def run
+  def run(pomodoro_length)
     Pommy.init_curses
-    while true
+    now = Time.now
+    future = now + pomodoro_length.to_f*60
+    while now < future
       now = Time.now
-      future = now + 25*60
-      while now < future
-        now = Time.now
-        difference = future - now
-        difference = 0 if difference < 0
-        minutes = (difference/60).to_i
-        seconds = (difference%60).floor
-        seconds = "0#{seconds}" if seconds < 10
-        percentage = (difference/(25*60/100))
-        negative_percentage = 100 - percentage
-        text = "#{"%3.2f" % negative_percentage}% #{minutes}:#{seconds}"
-        Pommy.reset_screen
-        Curses.addstr 'Pomodoro'
-        Curses.setpos(2,0)
-        Pommy.draw_bar(percentage, text)
-        Curses.refresh
-        sleep(0.1)
-      end
-      Curses.setpos(3,0)
-      Curses.addstr 'Pomodoro ended. Press a key to restart'
+      difference = future - now
+      difference = 0 if difference < 0
+      minutes = (difference/60).to_i
+      seconds = (difference%60).ceil
+      seconds = "0#{seconds}" if seconds < 10
+      percentage = (difference/(pomodoro_length.to_f*60/100))
+      negative_percentage = 100 - percentage
+      text = "#{"%3.2f" % negative_percentage}% #{minutes}:#{seconds}"
+      Pommy.reset_screen
+      Curses.addstr 'Pommy'
+      Curses.setpos(2,0)
+      Pommy.draw_bar(percentage, text)
       Curses.refresh
-      Curses.getch
+      sleep(0.1)
     end
+    Curses.setpos(3,0)
+    Curses.addstr 'Pommy ended. Press a key to close the screen.'
+    Curses.refresh
+    Curses.getch
   end
 end
